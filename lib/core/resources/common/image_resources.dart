fbfbbe9750215163ccdf27c5756e02e8123c054c
rsagar024 <ssahu4053@gmail.com>
2024-12-10 22:54:59 +0530

    added image resources in image resource class.

---
+++ lib/core/resources/common/image_resources.dart
@@ -0,0 +1,40 @@
+class ImageResources {
+  static const String _imageAssetsResources = 'assets/images';
+  static const String _iconAssetsResources = 'assets/icons';
+  static const String _riveAssetsResources = 'assets/rives';
+
+  // Icons
+  static const String iconBack = '$_iconAssetsResources/ic_back.svg';
+  static const String iconMore = '$_iconAssetsResources/ic_more.svg';
+  static const String iconEmail = '$_iconAssetsResources/ic_email.svg';
+  static const String iconPassword = '$_iconAssetsResources/ic_password.svg';
+  static const String iconPerson = '$_iconAssetsResources/ic_person.svg';
+  static const String iconFile = '$_iconAssetsResources/ic_file.svg';
+  static const String iconUpload = '$_iconAssetsResources/ic_upload.svg';
+  static const String iconProfile = '$_iconAssetsResources/ic_profile.svg';
+  static const String iconPlay = '$_iconAssetsResources/ic_play.svg';
+  static const String iconPause = '$_iconAssetsResources/ic_pause.svg';
+  static const String iconNext = '$_iconAssetsResources/ic_next.svg';
+  static const String iconPrevious = '$_iconAssetsResources/ic_prev.svg';
+  static const String iconPlaylist = '$_iconAssetsResources/ic_playlist.svg';
+  static const String iconShuffle = '$_iconAssetsResources/ic_shuffle.svg';
+  static const String iconEq = '$_iconAssetsResources/ic_eq.svg';
+  static const String iconRepeat = '$_iconAssetsResources/ic_repeat.svg';
+  static const String iconFavorite = '$_iconAssetsResources/ic_favorite.svg';
+
+  // Images
+  static const String imageFacebook = '$_imageAssetsResources/image_facebook.svg';
+  static const String imageGithub = '$_imageAssetsResources/image_github.svg';
+  static const String imageGoogle = '$_imageAssetsResources/image_google.svg';
+  static const String imagePerson = '$_imageAssetsResources/image_person.jpg';
+  static const String imageSpline = '$_imageAssetsResources/image_spline.png';
+
+  // Rives
+  static const String riveButton = '$_riveAssetsResources/button.riv';
+  static const String riveCheck = '$_riveAssetsResources/check.riv';
+  static const String riveConfetti = '$_riveAssetsResources/confetti.riv';
+  static const String riveHouse = '$_riveAssetsResources/house.riv';
+  static const String riveIcons = '$_riveAssetsResources/icons.riv';
+  static const String riveMenuButton = '$_riveAssetsResources/menu_button.riv';
+  static const String riveShapes = '$_riveAssetsResources/shapes.riv';
+}
