dfa99e0848143ef9251ce3542e19df5c79902b1c
rsagar024 <ssahu4053@gmail.com>
2024-12-13 00:16:15 +0530

    updated music card widget design.

--- lib/features/audio/presentation/widgets/music_card_widget.dart
+++ lib/features/audio/presentation/widgets/music_card_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vap_uploader/core/resources/common/image_resources.dart';
+import 'package:vap_uploader/core/resources/themes/text_styles.dart';
 import 'package:vap_uploader/features/audio/presentation/pages/music_player_page.dart';
 
 class MusicCardWidget extends StatelessWidget {
@@ -8,36 +10,85 @@ class MusicCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 5),
-      child: GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const MusicPlayerPage()));
-        },
-        child: ListTile(
-          contentPadding: const EdgeInsets.symmetric(horizontal: 12),
-          leading: const CircleAvatar(
-            backgroundImage: AssetImage(ImageResources.imagePerson),
-            radius: 30,
-          ),
-          title: const Text(
-            'Be the girl',
-            style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          subtitle: const Text(
-            'Bebe Rexa',
-            style: TextStyle(color: Colors.white70, fontSize: 14),
-          ),
-          trailing: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(width: 2, color: const Color(0xFFFF007F)),
+      padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const MusicPlayerPage()));
+            },
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image.asset(
+                    ImageResources.imagePerson,
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  height: 60,
+                  width: 60,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border.all(color: Colors.white60, width: 2),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Container(
+                    height: 15,
+                    width: 15,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.play_arrow, color: Colors.white),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => const MusicPlayerPage()));
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Milenge Hum Nahi',
+                              style: CustomTextStyles.custom13SemiBold,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              'Kunaal Verma',
+                              style: CustomTextStyles.custom10Regular,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset(ImageResources.iconPlayColored, height: 30),
+                    ),
+                  ],
+                ),
+                Divider(color: Colors.grey[800]),
+              ],
             ),
           ),
-        ),
+        ],
       ),
     );
   }
