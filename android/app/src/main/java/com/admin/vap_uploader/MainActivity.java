cbcc1b2051168af4cdd68764a64a9016df411791
rsagar024 <ssahu4053@gmail.com>
2024-12-19 19:31:30 +0530

    migrate android folder in kotlin to java.

---
+++ android/app/src/main/java/com/admin/vap_uploader/MainActivity.java
@@ -0,0 +1,6 @@
+package com.admin.vap_uploader;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/admin/vap_uploader/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.admin.vap_uploader
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
